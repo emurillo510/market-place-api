37eec785dfcdac319f5ddc987b61838d2dc5ac12
Elton Murillo <emurillo510@gmail.com>
2015-04-14 23:46:15 -0700

    Adds documentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
-
+  # Comes from app/controller/concerns/authenticable.rb
   include Authenticable
 
 end
