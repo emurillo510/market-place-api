01a4aa44a925aba6b6a1b70f34726efc54f2b3a1
Elton Murillo <emurillo510@gmail.com>
2015-04-14 19:13:33 -0700

    Adds devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
